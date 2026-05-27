d5aeaf06e452a45d1ebb3da6108611d282d2976b
Arturo Perez <jesusarturoperez1@gmail.com>
2026-05-27 15:54:03 -0600

    api rest, test, jest

--- server/app.js
+++ server/app.js
@@ -21,7 +21,12 @@ app.use("/api", indexRoutes);
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
 
+const birdRoutes = require("./routes/bird.routes");
+app.use("/api", birdRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 module.exports = app;
